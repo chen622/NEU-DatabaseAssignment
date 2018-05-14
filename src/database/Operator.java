f0cd41f04eb928d288b0fc65261219339719c6f4
Cheelem <cheelem@outlook.com>
2018-05-14 16:31:30 +0800

    XXXXXXXXXXXXXXX ccm

--- src/database/Operator.java
+++ src/database/Operator.java
@@ -1,5 +1,6 @@
 package database;
 
+import data.Account;
 import data.DVD;
 import data.Rental;
 
@@ -31,6 +32,7 @@ public class Operator {
                 + (genre == null || genre.equals("") ? "" : "genre=" + genre));
 
         if(whereCondition.endsWith("AND"))whereCondition.substring(whereCondition.length()-4,whereCondition.length()-1);
+        if(whereCondition.endsWith("WHERE "))whereCondition="";
         // WHERE CONDITION 直接写为数据库的条件 为空则返回所有
         ResultSet resultSet = database.executeQuery(
                 "SELECT *, (SELECT GROUP_CONCAT(cast.cast) FROM cast WHERE cast.prop_id=dvd_entity.prop_id) as cast, (SELECT GROUP_CONCAT(genre_type.genre_type_name) as genres FROM genre_type,dvd_genre WHERE genre_type.genre_type_id=dvd_genre.genre_type_id AND dvd_genre.prop_id=dvd_entity.prop_id) as genre FROM dvd_entity JOIN dvd_prop ON dvd_entity.prop_id=dvd_prop.prop_id "
@@ -60,7 +62,7 @@ public class Operator {
                 + (memberID == null || memberID.equals("") ? "" : "member_id=" + memberID));
 
         if(whereCondition.endsWith("AND"))whereCondition.substring(whereCondition.length()-4,whereCondition.length()-1);
-
+        if(whereCondition.endsWith("WHERE "))whereCondition="";
         ResultSet resultSet = database.executeQuery(
                 "SELECT * FROM rental,member,dvd_entity,dvd_prop WHERE rental.member_id=member.member_id AND rental.entity_id=dvd_entity.entity_id AND dvd_entity.prop_id=dvd_prop.prop_id"
                         + ((whereCondition == null || whereCondition.equals("")) ? "" : whereCondition)
@@ -79,5 +81,23 @@ public class Operator {
         return rentals;
     }
 
-    
+    public static Account[] accounts(Integer memberID) throws SQLException {
+        ResultSet resultSet = database.executeQuery(
+                "SELECT member.member_id,member_name,member_address,category,balance,COUNT(*) AS total_times,SUM(money) AS amount_paid FROM rental LEFT JOIN member ON rental.member_id=member.member_id"
+                + (memberID==null?"":"WHERE member.member_id=1")
+        );
+        Account[] accounts = new Account[resultSet.getFetchSize()];
+        int i = 0;
+        while (resultSet.next()) {
+            accounts[i] = new Account(
+                    resultSet.getString("member_name"),
+                    resultSet.getString("category"),
+                    resultSet.getFloat("balance"),
+                    resultSet.getFloat("amount_paid"),
+                    resultSet.getInt("total_times")
+            );
+            i++;
+        }
+        return accounts;
+    }
 }
